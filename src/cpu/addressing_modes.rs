d83e45553d97e88b5583f6d103c8b7a68756e70a
Matthew Dibble <mdibble@rogers.com>
2020-08-11 16:37:01 -0400

    Redid addressing modes

--- src/cpu/addressing_modes.rs
+++ src/cpu/addressing_modes.rs
@@ -1,168 +1,150 @@
 use crate::cpu::CPU;
 
 impl CPU {
-    pub fn set_mode(&mut self, mode: Mode) -> bool {
-        let mut extend = false;
-        match mode {
-            Mode::IMM => extend = self.mode_imm(), 
-            Mode::ZP => extend = self.mode_zp(), 
-            Mode::ZPX => extend = self.mode_zpx(), 
-            Mode::ZPY => extend = self.mode_zpy(), 
-            Mode::IZX => extend = self.mode_izx(), 
-            Mode::IZY => extend = self.mode_izy(), 
-            Mode::ABS => extend = self.mode_abs(), 
-            Mode::ABX => extend = self.mode_abx(), 
-            Mode::ABY => extend = self.mode_aby(),
-            Mode::ACC => extend = self.mode_acc(),
-            Mode::IND => extend = self.mode_ind(), 
-            Mode::REL => extend = self.mode_rel(), 
-            Mode::IMP => extend = self.mode_imp()
-        }
-        extend
+    pub fn set_mode(&mut self, mode: Mode) -> u16 {
+        let address: u16 = match mode {
+            Mode::IMM => self.mode_imm(), 
+            Mode::ZP => self.mode_zp(), 
+            Mode::ZPX => self.mode_zpx(), 
+            Mode::ZPY => self.mode_zpy(), 
+            Mode::IZX => self.mode_izx(), 
+            Mode::IZY => self.mode_izy(), 
+            Mode::ABS => self.mode_abs(), 
+            Mode::ABX => self.mode_abx(), 
+            Mode::ABY => self.mode_aby(),
+            Mode::ACC => self.mode_acc(),
+            Mode::IND => self.mode_ind(), 
+            Mode::REL => self.mode_rel(), 
+            Mode::IMP => self.mode_imp()
+        };
+        address
     }
 
-    pub fn mode_abs(&mut self) -> bool {
-        let lo = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-        let hi = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-
-        self.addr_abs = (hi << 8) | lo;
-
-        false
+    // Absolute
+    // 16-bit address is retrived from the next two bytes in the program counter
+    pub fn mode_abs(&mut self) -> u16 {
+        let left = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        let right = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        left << 4 | right
     }
 
-    pub fn mode_abx(&mut self) -> bool {
-        let lo = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-        let hi = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
+    // Absolute X
+    // 16-bit address is retrived from the next two bytes in the program counter offset by X
+    pub fn mode_abx(&mut self) -> u16 {
+        let left = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        let right = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        (left << 4 | right) + self.x as u16
+    }
 
-        self.addr_abs = (hi << 8) | lo;
-        self.addr_abs += self.x as u16;
+    // Absolute Y
+    // 16-bit address is retrived from the next two bytes in the program counter offset by Y
+    pub fn mode_aby(&mut self) -> u16 {
+        let left = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        let right = self.bus.get_memory(self.pc) as u16;
+        self.pc_increase();
+        (left << 4 | right) + self.y as u16
+    }
 
-        if (self.addr_abs & 0xFF00) != (hi << 8) {
-            true
-        }
+    // Accumulator
+    // Address is the accumulator, so no need to seek
+    pub fn mode_acc(&mut self) -> u16 {
+        self.pc_increase();
+        0
+    }
+
+    // Immediate
+    // Uses the address of the program counter for operation
+    pub fn mode_imm(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc_increase();
+        pc
+    }
+
+    // Implied
+    // Address is implied, so no need to seek
+    pub fn mode_imp(&mut self) -> u16 {
+        self.pc_increase();
+        0
+    }
+
+    // Indirect
+    // Need to learn more about this mode, pretty confusing
+    pub fn mode_ind(&mut self) -> u16 {
+        let address = self.bus.get_memory(self.pc + 1) as u16;
+        let low = self.bus.get_memory(address) as u16;
+        let high = if address & 0xFF == 0xFF {
+            self.bus.get_memory(address as u16 - 0xFF) as u16
+        } 
         else {
-            false
-        }
+            self.bus.get_memory(address + 1) as u16
+        };
+
+        (high << 8) | low
     }
 
-    pub fn mode_aby(&mut self) -> bool {
-        let lo = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-        let hi = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
+    // Indirect X-indexed
+    // I thought indirect was bad
+    pub fn mode_izx(&mut self) -> u16 {
+        let address = self.bus.get_memory(self.pc + 1);
+        let zp_low = address.wrapping_add(self.x);
+        let zp_high = zp_low.wrapping_add(1);
+        let zp_low_value = self.bus.get_memory(zp_low as u16) as u16;
+        let zp_high_value = self.bus.get_memory(zp_high as u16) as u16;
 
-        self.addr_abs = (hi << 8) | lo;
-        self.addr_abs += self.y as u16;
-
-        if (self.addr_abs & 0xFF00) != (hi << 8) {
-            true
-        }
-        else {
-            false
-        }
+        (zp_high_value << 8) | zp_low_value
     }
 
-    // Don't know if this will work
-    pub fn mode_acc(&mut self) -> bool {
-        self.addr_abs = self.pc;
-        self.pc += 1;
-        false
+    // Indirect Y-indexed
+    // I thought indirect was bad
+    pub fn mode_izy(&mut self) -> u16 {
+        let address = self.bus.get_memory(self.pc + 1);
+        let zp_low = address;
+        let zp_high = zp_low.wrapping_add(1);
+        let zp_low_value = self.bus.get_memory(zp_low as u16) as u16;
+        let zp_high_value = self.bus.get_memory(zp_high as u16) as u16;
+
+        let old_address = (zp_high_value << 8) | zp_low_value;
+        let new_address = old_address.wrapping_add(self.y as u16);
+
+        new_address
     }
 
-    pub fn mode_imm(&mut self) -> bool {
-        self.addr_abs = self.pc;
-        self.pc += 1;
-        false
+    // Relative
+    // Address is applied an offset from the PC
+    pub fn mode_rel(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc_increase();
+        self.pc + self.bus.get_memory(pc) as u16 // May cause overflow issues
+        // Should return a signed byte, which it does not.
     }
 
-    pub fn mode_imp(&mut self) -> bool {
-        self.fetched = self.a;
-        false
+    // Zero Page
+    // Address is fetched from 8-bit address on the zero page
+    pub fn mode_zp(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc_increase();
+        self.bus.get_memory(pc) as u16
     }
 
-    pub fn mode_ind(&mut self) -> bool {
-        let ptr_lo = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-        let ptr_hi = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-
-        let ptr = (ptr_hi << 8) | ptr_lo;
-
-        if ptr_lo == 0x00FF {
-            self.addr_abs = (self.bus.get_memory(ptr & 0xFF00) as u16) << 8 | self.bus.get_memory(ptr + 0) as u16;
-        }
-
-        else {
-            self.addr_abs = (self.bus.get_memory(ptr + 1) as u16) << 8 | self.bus.get_memory(ptr + 0) as u16;
-        }
-        
-        false
+    // Zero Page X-indexed
+    // Address is fetched from 8-bit address on the zero page, offset by X
+    pub fn mode_zpx(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc_increase();
+        self.bus.get_memory((pc + self.x as u16) % 256) as u16
     }
 
-    pub fn mode_izx(&mut self) -> bool {
-        let t = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-
-        let lo: u16 = self.bus.get_memory((t + self.x as u16) & 0x00FF).into();
-        let hi: u16 = self.bus.get_memory((t + (self.x + 1) as u16) & 0x00FF).into();
-
-        self.addr_abs = (hi << 8) | lo;
-
-        false
-    }
-
-    pub fn mode_izy(&mut self) -> bool {
-        let t = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-
-        let lo: u16 = self.bus.get_memory(t & 0x00FF).into();
-        let hi: u16 = self.bus.get_memory((t + 1) & 0x00FF).into();
-
-        self.addr_abs = (hi << 8) | lo;
-        self.addr_abs += self.y as u16;
-
-        if (self.addr_abs & 0xFF00) != (hi << 8) {
-            true
-        }
-        else {
-            false
-        }
-    }
-
-    pub fn mode_rel(&mut self) -> bool {
-        self.addr_rel = self.bus.get_memory(self.pc) as u16;
-        self.pc += 1;
-
-        if self.addr_rel & 0x80 != 0 {
-            self.addr_rel |= 0xFF00
-        }
-        
-        false
-    }
-
-    pub fn mode_zp(&mut self) -> bool {
-        self.addr_abs = self.bus.get_memory(self.pc).into();
-        self.pc += 1;
-        self.addr_abs &= 0x00FF;
-        false
-    }
-
-    pub fn mode_zpx(&mut self) -> bool {
-        self.addr_abs = self.bus.get_memory(self.pc + self.x as u16).into();
-        self.pc += 1;
-        self.addr_abs &= 0x00FF;
-        false
-    }
-
-    pub fn mode_zpy(&mut self) -> bool {
-        self.addr_abs = self.bus.get_memory(self.pc + self.y as u16).into();
-        self.pc += 1;
-        self.addr_abs &= 0x00FF;
-        false
+    // Zero Page Y-indexed
+    // Address is fetched from 8-bit address on the zero page, offset by Y
+    pub fn mode_zpy(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc_increase();
+        self.bus.get_memory((pc + self.y as u16) % 256) as u16
     }
 }
 
--- src/cpu/opcodes.rs
+++ src/cpu/opcodes.rs
@@ -2,227 +2,227 @@ use crate::cpu::CPU;
 use super::addressing_modes::Mode;
 
 impl CPU {
-    pub fn adc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn adc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn and(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn and(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn asl(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn asl(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bcc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bcc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bcs(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bcs(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn beq(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn beq(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bit(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bit(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bmi(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bmi(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bne(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bne(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bpl(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bpl(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn brk(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn brk(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bvc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bvc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn bvs(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn bvs(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn clc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn clc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn cld(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn cld(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn cli(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn cli(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn clv(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn clv(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn cmp(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn cmp(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn cpx(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn cpx(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn cpy(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn cpy(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn dec(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn dec(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn dex(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn dex(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn dey(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn dey(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn eor(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn eor(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn inc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn inc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn inx(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn inx(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn iny(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn iny(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn jmp(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn jmp(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn jsr(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn jsr(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn lda(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn lda(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn ldx(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn ldx(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn ldy(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn ldy(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn lsr(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn lsr(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn nop(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn nop(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn ora(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn ora(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn pha(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn pha(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn rol(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn rol(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn ror(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn ror(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn rti(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn rti(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn rts(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn rts(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn php(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn php(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn pla(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn pla(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn plp(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn plp(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sbc(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sbc(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sec(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sec(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sed(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sed(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sei(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sei(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sta(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sta(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn stx(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn stx(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn sty(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn sty(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn tax(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn tax(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn tay(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn tay(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn tsx(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn tsx(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn txa(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn txa(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn txs(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn txs(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 
-    pub fn tya(&mut self, mode: Mode) -> bool {
-        self.set_mode(mode) & true
+    pub fn tya(&mut self, mode: Mode) {
+        self.set_mode(mode);
     }
 }
